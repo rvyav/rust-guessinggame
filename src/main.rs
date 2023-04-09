288aab61ad6871c590a176025bea8e7737742375
rvyav <rvyav@hotmail.com>
2023-04-08 20:06:28 -0400

    generate guessing number based on user input

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 // extern crate rand;
 
 use rand::Rng;
+use rand::random;
 use std::env;
 use std::io;
 
@@ -8,12 +9,25 @@ fn main() {
     // println!("Welcome to our Guessing game");
     // println!("Your task is to find the magic number between the range of the two numbers below that you will defined");
 
-    println!("Choose the start of the range number");
-    let mut range_start = String::new();
+    println!("Choose the start of the range number: ");
+    let mut input_range_start = String::new();
     io::stdin()
-        .read_line(&mut range_start)
+        .read_line(&mut input_range_start)
         .unwrap();
     
-    let trimmed: i32 = range_start.trim().parse().unwrap();
-    println!("hello {}", trimmed);
+    let range_start: i32 = input_range_start.trim().parse().unwrap();
+    
+
+    println!("Choose the end of the range number: ");
+    let mut input_range_stop = String::new();
+    io::stdin()
+        .read_line(&mut input_range_stop)
+        .unwrap();
+    
+    let range_stop: i32 = input_range_stop.trim().parse().unwrap();
+
+    println!("The numbers you selected are {} and {}", range_start, range_stop);
+
+    let random_number = rand::thread_rng().gen_range(range_start..range_stop);
+    println!("Guessing number is {}", random_number);
 }
